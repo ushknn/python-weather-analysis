09915ae250774ba4b7d042287e3e6b7be4a6d342
ushknn <uekiworks@gmail.com>
2024-09-19 19:54:27 +0900

    tokyo temp data loader

--- data_loader.py
+++ data_loader.py
@@ -19,3 +19,15 @@ def load_sst():
     m = sst_dataset['m']
 
     return sst, lon2, lat2, y, m
+
+
+def load_tokyo_temp():
+    tokyo_temp = np.genfromtxt("data/Tokyo_temp.csv",
+                              delimiter=",",
+                              usecols=(0,1,2)
+                             )
+    y = tokyo_temp[:, 0]
+    m = tokyo_temp[:, 1]
+    temp = tokyo_temp[:, 2]
+
+    return y, m, temp
